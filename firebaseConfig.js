a0eecd752bb9d3a426761f485b5f8040b60ba260
Caio-2611 <130412075+Caio-2611@users.noreply.github.com>
2026-04-15 00:21:33 +0000

    projeto

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
